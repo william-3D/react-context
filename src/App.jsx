fa4965140d6d5f12f3bd17f2ea44a2b499a07326
Sonia William <98wsonia@gmail.com>
2024-08-31 18:50:09 +0800

    remove context from main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState, useEffect, useContext, createContext } from 'react';
+import { useState, useEffect } from 'react';
 import RoundedCard from './components/RoundedCard';
 import DetailsSection from './components/DetailsSection';
 import Title from './components/Title';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { get } from './api'
 import './App.css'
-
-const CharacterList = ({ characterName }) => {
-  return (
-    <li>{characterName}</li>
-  )
-}
 
 function App() {
   const [data, setData] = useState([]);
@@ -28,9 +20,6 @@ function App() {
         setError(err.message);
       });
   }, []);
-
-  const CharacterContext = createContext({});
-
 
   return (
     <div className='flex gap-4 justify-center items-center'>
